36908bc68fdc29bda6b39398165439b2dd4c61bf
AlexGr <753464@gmail.com>
2022-09-24 18:27:17 +0300

    1.0.15

--- src/middlewares/blogId-validation-middleware.ts
+++ src/middlewares/blogId-validation-middleware.ts
@@ -1,6 +1,14 @@
 import { body } from 'express-validator';
+import blogsService from '../domain/blogs-service.js';
+
 
 export const blogIdValidationMiddleware = body('blogId')
+    .custom(async (val) => {
+        console.log('typeof val:', typeof val);
+        const result = await blogsService.readOne(val)
+        console.log('result:', !!result);
+        if (!result) throw Error('bloger not finded')
+    })
     .exists()
     .notEmpty({ ignore_whitespace: true })
     .isString()
